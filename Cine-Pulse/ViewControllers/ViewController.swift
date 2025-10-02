5c218ecd5a19c9e344c960c6e6fd2cf43b351c35
Rovshan Rasulov <soulofchinder.2@gmail.com>
2025-10-03 01:06:34 +0400

    first commit

---
+++ Cine-Pulse/ViewControllers/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Cine-Pulse
+//
+//  Created by Rovshan Rasulov on 03.10.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+}
+
